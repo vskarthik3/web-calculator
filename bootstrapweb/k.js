3cf9f2f28207d668c63b350578d142250e97ff1f
vskarthik3 <karthikvuchuru2000@gmail.com>
2022-09-25 14:25:25 +0530

    okey

---
+++ bootstrapweb/k.js
@@ -0,0 +1,13 @@
+var price= 499;
+var product_rating=4.5;
+var brand= "roadstar";
+var product_name= "slim fit shirt";
+var is_avail= true;
+var discount;
+var size=[30,35,38,40];
+var product_details={product_name:"Iphone14", price:60000};
+
+console.log(product_rating);
+console.log(discount);
+console.log(product_details);
+console.log(typeof is_avail);
